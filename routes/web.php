63e953e0ccda8e4748ba7f6d5367c5f0e5876edc
Ainun Ijlal <ijlalcode@gmail.com>
2026-02-16 03:58:36 +0800

    updateltk

--- routes/web.php
+++ routes/web.php
@@ -48,6 +48,7 @@ Route::middleware(['auth'])->group(function () {
 Route::get('ltk/{id}/pdf', [LtkController::class, 'downloadPDF'])->name('ltk.pdf');
 
 
+
     // Resource LTK (Staff juga butuh akses ini)
     Route::resource('ltk', LtkController::class)->except(['destroy']); // Sesuaikan jika staff tidak boleh hapus
 
